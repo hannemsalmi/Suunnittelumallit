d4eb1aed6aa2700c2bf9fdbdb7e1838102d4ed2b
Hanne Salmi <hanne.salmi@metropolia.fi>
2023-02-28 16:07:12 +0200

    Tehtävä 8

--- src/templatemethod/Main.java
+++ src/templatemethod/Main.java
@@ -2,7 +2,7 @@ package templatemethod;
 
 public class Main {
     public static void main(String[] args) {
-        GuessTheNumber game = new GuessTheNumber();
-        game.playOneGame(2);
+    	GuessTheNumber game = new GuessTheNumber();
+    	game.playOneGame(2);
     }
 }
