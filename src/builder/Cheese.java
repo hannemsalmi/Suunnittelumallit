bc5ba3ae6db32364c1a3e256d352d1c9a6c7d1ac
Hanne Salmi <hanne.salmi@metropolia.fi>
2023-03-29 21:34:45 +0300

    Tehtävä 14

---
+++ src/builder/Cheese.java
@@ -0,0 +1,13 @@
+package builder;
+
+public class Cheese {
+    private String name;
+
+    public Cheese(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
